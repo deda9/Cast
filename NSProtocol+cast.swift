da98b07a12fc81e662db954a83a29bafb0f80d35
deda9 <Bassem.Qoulta@gmail.com>
2018-11-14 13:10:25 +0100

    Update

--- NSProtocol+cast.swift
+++ NSProtocol+cast.swift
@@ -3,54 +3,54 @@ import Foundation
 
 extension NSObjectProtocol {
     
-    func cast<T, U>(_ view: T) -> U {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(#file), line: \(#line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T) -> U {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(#file), line: \(#line)"
+        return self.cast(object, message: message)
     }
     
-    func cast<T, U>(_ view: T, file: String = #file) -> U {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(file), line: \(#line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T, file: String = #file) -> U {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(file), line: \(#line)"
+        return self.cast(object, message: message)
     }
     
-    func cast<T, U>(_ view: T, file: String = #file, line: Int = #line) -> U {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(file), line: \(line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T, file: String = #file, line: Int = #line) -> U {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(file), line: \(line)"
+        return self.cast(object, message: message)
     }
     
-    private func cast<T, U>(_ view: T, message: @autoclosure () -> String) -> U {
-        guard let castedview = view as? U else {
+    private func cast<T, U>(_ object: T, message: @autoclosure () -> String) -> U {
+        guard let castedobject = object as? U else {
             fatalError(message)
         }
-        return castedview
+        return castedobject
     }
 }
 
 extension NSObjectProtocol {
     
-    func cast<T, U>(_ view: T?) -> U? {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(#file), line: \(#line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T?) -> U? {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(#file), line: \(#line)"
+        return self.cast(object, message: message)
     }
     
-    func cast<T, U>(_ view: T?, file: String = #file) -> U? {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(file), line: \(#line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T?, file: String = #file) -> U? {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(file), line: \(#line)"
+        return self.cast(object, message: message)
     }
     
-    func cast<T, U>(_ view: T?, file: String = #file, line: Int = #line) -> U? {
-        let message = "Couldn't cast object '\(String(describing: view))' to '\(U.self)' file: \(file), line: \(line)"
-        return self.cast(view, message: message)
+    func cast<T, U>(_ object: T?, file: String = #file, line: Int = #line) -> U? {
+        let message = "Couldn't cast object '\(String(describing: object))' to '\(U.self)' file: \(file), line: \(line)"
+        return self.cast(object, message: message)
     }
     
-    private func cast<T, U>(_ view: T?, message: @autoclosure () -> String) -> U? {
-        guard let nonOptional = view else {
+    private func cast<T, U>(_ object: T?, message: @autoclosure () -> String) -> U? {
+        guard let nonOptional = object else {
             return nil
         }
         
-        guard let castedview = nonOptional as? U else {
+        guard let castedobject = nonOptional as? U else {
             fatalError(message)
         }
-        return castedview
+        return castedobject
     }
 }
